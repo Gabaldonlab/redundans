9c0a25b21248ca88f2a0b02ec89226f39e2ca729
lpryszcz <l.p.pryszcz@gmail.com>
2016-09-08 17:47:47 +0200

    fasta2homozygous.py simplified

--- redundans.py
+++ redundans.py
@@ -414,7 +414,7 @@ def main():
                                       formatter_class=argparse.RawTextHelpFormatter)
   
     parser.add_argument("-v", "--verbose",  default=False, action="store_true", help="verbose")    
-    parser.add_argument('--version', action='version', version='0.12c')   
+    parser.add_argument('--version', action='version', version='0.12d')
     parser.add_argument("-i", "--fastq", nargs="+", required=1, 
                         help="FASTQ PE/MP files")
     parser.add_argument("-f", "--fasta", required=1, 
