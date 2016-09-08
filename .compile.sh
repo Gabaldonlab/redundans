6bbe56b4934fe27fd0d1aa52c0fd7afc6ee3cda6
lpryszcz <l.p.pryszcz@gmail.com>
2016-09-08 14:38:15 +0200

    make

--- .compile.sh
+++ .compile.sh
@@ -7,13 +7,15 @@ if [ ! -z $1 ]; then log=$1; fi
 echo `date` "Compiling dependencies..." 
 echo " === You can find log in: $log === "
 cores=`grep -c ^processor /proc/cpuinfo | awk '{if($1>1){print $1-1} else {print 1}}'`
-echo " I'll use $cores threads for compiling"
+echo " I'll use $cores thread(s) for compiling"
 
 echo `date` " BWA"
 (cd bin/bwa && make clean && make -j $cores) >> $log 2>&1 
+retcode=$?; if [ $retcode -gt 0 ]; then exit $retcode; fi
 
 echo `date` " LASTal"
 (cd bin/last && make clean && make -j $cores) >> $log 2>&1
+retcode=$?; if [ $retcode -gt 0 ]; then exit $retcode; fi
 
 echo `date` "Done!"
-exit 0
+
