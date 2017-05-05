b4364a98bd7b1a475e55121cba7cedae221e1255
lpryszcz <l.p.pryszcz@gmail.com>
2017-05-05 14:50:58 +0200

    v0.13c alpha; lastal instead of bwa/snap; simplified; faster; more accurate

---
+++ bin/fastq2shuffled.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os, sys, subprocess, gzip
+from itertools import izip
+
+def fqparser(fn, stripNames=0, pair="", i=0):
+    """Single process implementation of rawtrimmer.
+    Open zcat subprocess and read from stdin."""
+    if fn.endswith('.gz'):
+        zcat = subprocess.Popen(['zcat', fn], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        handle = zcat.stdout
+    else:
+        handle = open(fn)
+        
+    fqlist = []
+    for l in handle:
+        if not l[:-1]:
+            continue
+        fqlist.append(l[:-1])
+        if len(fqlist) == 4:
+            i += 1
+            name, seq, sep, quals = fqlist
+            if stripNames: 
+                yield "@%s%s\n%s\n%s\n%s\n"%(i, pair, seq, sep, quals)
+            else:
+                yield "%s%s\n%s\n%s\n%s\n"%(name, pair, seq, sep, quals)
+            fqlist = []
+
+def fastq2shuffled(fnames, out=sys.stdout, stripNames=1, limit=0):
+    """Process FastQ files"""
+    i = 0
+    for fn1, fn2 in zip(fnames[::2], fnames[1::2]):
+        for i, (fq1, fq2) in enumerate(izip(fqparser(fn1, stripNames, '/1', i), fqparser(fn2, stripNames, '/2', i)), i):
+            if limit and i>limit:
+                break
+            out.write(fq1+fq2)
+        
+if __name__=="__main__":
+    fastq2shuffled(sys.argv[1:])
