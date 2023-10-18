742e70e5e6f311d3e1b9fca35f95192eb892de92
Diego Fuentes Palacios <43205584+Dfupa@users.noreply.github.com>
2023-10-18 12:07:42 +0200

    Commented deprecated commands in .compile.sh
    Commented the commands to obtain K8 v0.23 and Platanus as they are provided by this repo for bioconda-recipes integration

--- bin/.compile.sh
+++ bin/.compile.sh
@@ -14,9 +14,9 @@ echo " === You can find log in: $log === "
 cores=`grep -c ^processor /proc/cpuinfo | awk '{if($1>1){print $1-1} else {print 1}}'`
 echo " I'll use $cores thread(s) for compiling"
 
-echo `date` " Platanus"
-(wget -O- http://platanus.bio.titech.ac.jp/?ddownload=145 > bin/platanus && chmod +x bin/platanus) >> $log 2>&1 
-retcode=$?; if [ $retcode -gt 0 ]; then exit $retcode; fi
+#echo `date` " Platanus"
+#(wget -O- http://platanus.bio.titech.ac.jp/?ddownload=145 > bin/platanus && chmod +x bin/platanus) >> $log 2>&1 
+#retcode=$?; if [ $retcode -gt 0 ]; then exit $retcode; fi
 
 echo `date` " Miniasm"
 (cd bin/miniasm && make clean && make) >> $log 2>&1
@@ -40,9 +40,9 @@ echo `date` " Minimap2"
 (cd bin/minimap2 && make clean && make) >> $log 2>&1
 retcode=$?; if [ $retcode -gt 0 ]; then exit $retcode; fi
 
-echo `date` " k8"
-(cd bin && curl -L https://github.com/attractivechaos/k8/releases/download/v0.2.4/k8-0.2.4.tar.bz2 | tar -jxf - && mv -t . ./k8-0.2.4/k8-Linux && rm -r ./k8-0.2.4/) >> $log 2>&1
-retcode=$?; if [ $retcode -gt 0 ]; then exit $retcode; fi
+#echo `date` " k8"
+#(cd bin && curl -L https://github.com/attractivechaos/k8/releases/download/v0.2.4/k8-0.2.4.tar.bz2 | tar -jxf - && mv -t . ./k8-0.2.4/k8-Linux && rm -r ./k8-0.2.4/) >> $log 2>&1
+#retcode=$?; if [ $retcode -gt 0 ]; then exit $retcode; fi
 
 echo `date` " Meryl"
 (cd bin && wget -nc https://github.com/marbl/meryl/releases/download/v1.3/meryl-1.3.Linux-amd64.tar.xz && tar -xJf meryl-1.3.Linux-amd64.tar.xz && mv -t . ./meryl-1.3/bin/*  && rm -r ./meryl-1.3/ && rm meryl-1.3.Linux-amd64.tar.xz) >> $log 2>&1
